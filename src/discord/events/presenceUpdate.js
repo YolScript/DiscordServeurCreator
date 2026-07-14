b362d9f4b1fddeaf3c8f7aeb4f4b8a962e070914
energiecraftonline-afk <energiecraftonline-afk@users.noreply.github.com>
2026-07-14 16:37:33 +0200

    fix: crash a chaque presenceUpdate (staffVoiceManager.syncHub inexistant)
    TypeError non catche (throw synchrone avant que .catch() puisse
    s'attacher) sur chaque evenement de presence, faisant planter tout le
    process. syncHub etait un reliquat d'une iteration anterieure du
    systeme SERVICE STAFF, jamais nettoye apres le passage au systeme de
    toggle actuel (base sur voiceStateUpdate, pas presenceUpdate). Retire
    l'appel mort et l'import inutilise.

--- src/discord/events/presenceUpdate.js
+++ src/discord/events/presenceUpdate.js
@@ -1,7 +1,6 @@
 const { Events, ActivityType } = require('discord.js');
 const client = require('../client');
 const { ensureRoleForGame } = require('../roles/gameRoleManager');
-const staffVoiceManager = require('../roles/staffVoiceManager');
 const logger = require('../../shared/logger');
 
 client.on(Events.PresenceUpdate, (oldPresence, newPresence) => {
@@ -11,6 +10,4 @@ client.on(Events.PresenceUpdate, (oldPresence, newPresence) => {
   for (const activity of playing) {
     ensureRoleForGame(newPresence.guild, activity.name).catch((err) => logger.error('ensureRoleForGame', err));
   }
-
-  staffVoiceManager.syncHub(newPresence.guild).catch((err) => logger.error('syncHub', err));
 });
